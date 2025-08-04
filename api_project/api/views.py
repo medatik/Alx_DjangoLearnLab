2a8c1d8b6f29b779e40cb94e90a7944cb2deb314
medatik <mohamed.atikeddine@gmail.com>
2025-08-04 10:08:31 +0100

    Implement token authentication and permissions for Book API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework import permissions
 
 
 
@@ -13,5 +14,6 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
