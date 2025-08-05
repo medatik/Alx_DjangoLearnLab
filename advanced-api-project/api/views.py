7bbe8a45f90199359afdd9f3596b15a2e44d9c08
medatik <mohamed.atikeddine@gmail.com>
2025-08-05 10:09:25 +0100

    correction 3

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 # Accessible sans authentification
 class BookListView(generics.ListAPIView):
